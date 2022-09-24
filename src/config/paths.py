5c1259832df315bdfe33c4d28c0f32abd6f9c4a9
adamcihlar <adam.cihlar3@gmail.com>
2022-09-24 13:59:40 +0200

    Paths to data

---
+++ src/config/paths.py
@@ -0,0 +1,23 @@
+import os
+
+DATA = "data"
+
+# RAW
+DATA_RAW = os.path.join(DATA, "raw")
+
+DATA_RAW_ZIP_FACEBOOK = os.path.join(DATA_RAW, "facebook.zip")
+DATA_RAW_ZIP_CSFD = os.path.join(DATA_RAW, "csfd.zip")
+DATA_RAW_ZIP_MALL = os.path.join(DATA_RAW, "mall.zip")
+
+DATA_RAW_DIR_FACEBOOK = os.path.join(DATA_RAW, "facebook")
+DATA_RAW_DIR_CSFD = os.path.join(DATA_RAW, "csfd")
+DATA_RAW_DIR_MALL = os.path.join(DATA_RAW, "mall")
+
+# PROCESSED
+DATA_PROCESSED = os.path.join(DATA, "preprocessed")
+
+DATA_PROCESSED_FACEBOOK = os.path.join(DATA_PROCESSED, "facebook.csv")
+DATA_PROCESSED_CSFD = os.path.join(DATA_PROCESSED, "csfd.csv")
+DATA_PROCESSED_MALL = os.path.join(DATA_PROCESSED, "mall.csv")
+
+DATA_PROCESSED_CONCAT = os.path.join(DATA_PROCESSED, "concat_dataset.csv")
