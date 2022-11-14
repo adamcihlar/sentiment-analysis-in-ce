8964d0f32b705c4d9530b261272922188599b67b
adamcihlar <adam.cihlar3@gmail.com>
2022-11-14 10:56:16 +0100

    Init encoders script

--- src/model/encoders.py
+++ src/model/encoders.py
@@ -1,1 +1,11 @@
 from transformers import RobertaModel
+
+
+class Encoder:
+    """
+    Easy initialization of the correct encoder.
+    Simplification of the forward method to output only the embeddings.
+    """
+
+    def __init__(self):
+        raise NotImplementedError
