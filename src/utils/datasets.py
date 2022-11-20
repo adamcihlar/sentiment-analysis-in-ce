00e0cb031956bc445d25e5ae17cef6ca77cd3ffe
adamcihlar <adam.cihlar3@gmail.com>
2022-11-20 17:13:14 +0100

    Bulk source finetuning datasets preprocessing

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -2,6 +2,7 @@ import pandas as pd
 import torch
 from typing import List
 from sklearn.model_selection import train_test_split
+from loguru import logger
 
 from src.config.parameters import RANDOM_STATE
 
@@ -11,19 +12,40 @@ def drop_undefined_classes(dataset: pd.DataFrame):
     return dataset
 
 
+def transform_labels_to_probs(dataset, drop_neutral=True):
+    """
+    Based on how I want to finetune the source classifier, I need to transform the labels.
+    I can either drop the neutral class to have binary classification or make it 0.5 and finetune in distilationlike settings.
+    """
+    if drop_neutral:
+        sum_neutral = sum(dataset.label == 2)
+        logger.info(
+            f"Dropped neutral class - {sum_neutral} samples, data transformed to binary classification problem."
+        )
+        dataset = dataset.loc[dataset.label.isin([0, 1])]
+    else:
+        logger.info(
+            f"Neutral class labelled as 0.5, data transformed to distilationlike settings."
+        )
+        dataset.label.loc[dataset.label == 2] = 0.5
+    return dataset
+
+
 def get_finetuning_datasets(source_dataset: pd.DataFrame):
     """
     Getting the dataset for finetuning is not dependant on the target dataset.
     Basically just splits the source dataset to train and val.
     """
     source_train_X, source_val_X, source_train_y, source_val_y = train_test_split(
-        source_dataset.text,
+        source_dataset.loc[:, source_dataset.columns != "label"],
         source_dataset.label,
         test_size=0.2,
         random_state=RANDOM_STATE,
     )
-    source_train = pd.DataFrame({"text": source_train_X, "label": source_train_y})
-    source_val = pd.DataFrame({"text": source_val_X, "label": source_val_y})
+    #     source_train = pd.DataFrame({"text": source_train_X, "label": source_train_y})
+    #     source_val = pd.DataFrame({"text": source_val_X, "label": source_val_y})
+    source_train = pd.concat([source_train_X, source_train_y], axis=1)
+    source_val = pd.concat([source_val_X, source_val_y], axis=1)
     return source_train, source_val
 
 
@@ -40,6 +62,8 @@ def get_adaptation_datasets(
     how the target encoder and classier are performing on that.
     Sample the source train dataset based on the length of target dataset
     to get as much information as possible from the target while adapting.
+
+    Get the first input by calling get_predictions method of ClassificationDataset.
 
     Returns:
         adaptation_source_train, that will be used in both adaptation steps, but with different labels
@@ -63,7 +87,7 @@ def get_adaptation_datasets(
 class Dataset(torch.utils.data.Dataset):
     """Keeps data in format accepted by the torch Dataloader."""
 
-    def __init__(self, encodings, labels=None):
+    def __init__(self, encodings, labels=None, source=None):
         self.encodings = encodings
         self.labels = labels
 
@@ -87,7 +111,8 @@ class ClassificationDataset:
     def __init__(
         self,
         X: pd.Series,
-        y: pd.Series,
+        y: pd.Series = None,
+        source: pd.Series = None,
     ):
         """
         Stores the texts and labels.
@@ -98,24 +123,7 @@ class ClassificationDataset:
         self.X_tok = None
         self.X_preprocessed = None
         self.y_pred = None
-
-    def transform_labels_to_probs(self, drop_neutral=True):
-        """
-        Based on how I want to finetune the source classifier, I need to transform the labels.
-        I can either drop the neutral class to have binary classification or make it 0.5 and finetune in distilationlike settings.
-        """
-        if drop_neutral:
-            logger.info(
-                f"Dropped neutral class, data transformed to binary classification problem."
-            )
-            self.X = self.X.loc[self.y.isin([0, 1])]
-            self.y = self.y.loc[self.y.isin([0, 1])]
-        else:
-            logger.info(
-                f"Neutral class labelled as 0.5, data transformed to distilationlike settings."
-            )
-            self.y.loc[self.y == 2] = 0.5
-        pass
+        self.source = source
 
     def report_metric(self, metric):
         """
@@ -129,7 +137,7 @@ class ClassificationDataset:
         The main functionality will be to get the meat from the emails.
         """
         self.X_preprocessed = preprocessor.preprocess(self.X)
-        return self.X_preprocessed
+        pass
 
     def tokenize(
         self, tokenizer, padding="max_length", max_length=512, truncation=True
@@ -137,20 +145,19 @@ class ClassificationDataset:
         """
         Returns tokenized inputs and stores them at the same time.
         """
-        self.X_tok = tokenizer.model(
-            self.X_preprocessed,
-            padding=padding,
+        self.X_tok = tokenizer.tokenize(
+            list(self.X_preprocessed),
             max_length=max_length,
-            truncation=truncation,
         )
-        return self.X_tok
-
-    def create_dataset(self):
-        self.torch_dataset = Dataset(self.X_tok, self.y)
         pass
 
-    def create_dataloader(self, batch_size, shuffle, num_workers):
-        self.torch_dataloader = DataLoader(
+    def create_dataset(self):
+        torch_y = list(self.y) if self.y is not None else None
+        self.torch_dataset = Dataset(self.X_tok, torch_y)
+        pass
+
+    def create_dataloader(self, batch_size=64, shuffle=True, num_workers=0):
+        self.torch_dataloader = torch.utils.data.DataLoader(
             self.torch_dataset,
             batch_size=batch_size,
             shuffle=shuffle,
@@ -167,3 +174,54 @@ class ClassificationDataset:
 
     def save_predictions(self):
         raise NotImplementedError
+
+
+def get_source_datasets_ready_for_finetuning(
+    datasets: List[pd.DataFrame],
+    drop_neutral=True,
+    preprocessor=Preprocessor(),
+    tokenizer=Tokenizer(),
+    batch_size=64,
+    shuffle=True,
+    num_workers=0,
+):
+    """
+    Just wrapping many functions and methods that are common for preparing
+    source finetuning train and validation datasets.
+    Takes list of source datasets, performs basic preprocessing operations,
+    splits them to train and validation parts and ClassificationDataset
+    instances from all of them with torch dataset and dataloader ready for
+    training/evaluation.
+    """
+    datasets = [drop_undefined_classes(ds) for ds in datasets]
+    datasets = [
+        transform_labels_to_probs(ds, drop_neutral=drop_neutral) for ds in datasets
+    ]
+    datasets = [get_finetuning_datasets(ds) for ds in datasets]
+    train_datasets, val_datasets = list(zip(*datasets))
+
+    train_datasets = [
+        ClassificationDataset(ds.text, ds.label, ds.source) for ds in train_datasets
+    ]
+    val_datasets = [
+        ClassificationDataset(ds.text, ds.label, ds.source) for ds in val_datasets
+    ]
+    [ds.preprocess(preprocessor) for ds in train_datasets]
+    [ds.tokenize(tokenizer) for ds in train_datasets]
+    [ds.create_dataset() for ds in train_datasets]
+    [
+        ds.create_dataloader(
+            batch_size=batch_size, shuffle=shuffle, num_workers=num_workers
+        )
+        for ds in train_datasets
+    ]
+    [ds.preprocess(preprocessor) for ds in val_datasets]
+    [ds.tokenize(tokenizer) for ds in val_datasets]
+    [ds.create_dataset() for ds in val_datasets]
+    [
+        ds.create_dataloader(
+            batch_size=batch_size, shuffle=shuffle, num_workers=num_workers
+        )
+        for ds in val_datasets
+    ]
+    return train_datasets, val_datasets
