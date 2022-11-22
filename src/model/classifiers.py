a0df565a5075fa0acaa4740ba3b3756cd1168f81
adamcihlar <adam.cihlar3@gmail.com>
2022-11-22 10:18:09 +0100

    Move finetunning to method

--- src/model/classifiers.py
+++ src/model/classifiers.py
@@ -111,9 +111,211 @@ class AdaptiveSentimentClassifier:
         val_datasets: Dict,
         optimizer: Type[torch.optim.Optimizer],
         optimizer_params: Dict,
-        lr_scheduler: Callable,
+        lr_params: Dict
+        lr_scheduler_call: Callable,
+        warmup_steps_proportion: float,
         num_epochs: int,
+        metrics: List,
     ):
+        # save start time of the training
+        start_time = time.strftime("%Y%m%d-%H%M%S")
+
+        # get one classfication head per dataset and shared encoder for all datasets
+        classifiers = {ds_name: self.classifier() for ds_name in train_datasets}
+        encoder = source_encoder
+
+        # get optimizer for each classfication head and the shared encoder
+        cls_optimizers = {
+            cls_name: optimizer(classifiers[cls_name].parameters(), **optimizer_params)
+            for cls_name in classifiers
+        }
+        if layer_wise_lr_decay is None or layer_wise_lr_decay == 1:
+            encoder_optimizer = optimizer(encoder.parameters(), **optimizer_params)
+        else:
+            list_of_layers = source_encoder.encoder.encoder.layer
+            optimizer_params_list = layer_wise_learning_rate(list_of_layers, optimizer_params["lr"], **lr_params)
+            encoder_optimizer = optimizer(optimizer_params_list, **optimizer_params)
+
+        # compute the total num of training steps to init lr_scheduler
+        num_steps_per_epoch_per_dataloader = {
+            ds_name: len(train_datasets[ds_name].torch_dataloader)
+            for ds_name in train_datasets
+        }
+        num_training_steps = (
+            sum(num_steps_per_epoch_per_dataloader.values()) * num_epochs
+        )
+
+        # get sequence of how batches will be taken from dataloaders
+        dl_samples = [
+            [dl] * num_steps_per_epoch_per_dataloader[dl]
+            for dl in num_steps_per_epoch_per_dataloader
+        ]
+        samples_schedule = [ds for sublist in dl_samples for ds in sublist]
+        random.shuffle(samples_schedule)
+
+        # get a lr_scheduler for every optimizer
+        all_optimizers_list = list(cls_optimizers.values()) + [encoder_optimizer]
+        lr_schedulers = [
+            lr_scheduler_call(
+                optimizer=optim,
+                num_warmup_steps=num_training_steps * warmup_steps_proportion,
+                num_training_steps=num_training_steps,
+            )
+            for optim in all_optimizers_list
+        ]
+
+        # put all models to device, gpu if available, else cpu
+        device = (
+            torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        )
+        [cls.to(device) for cls in classifiers.values()]
+        encoder.to(device)
+
+        # track training info
+        val_metrics = {metric: load(metric) for metric in metrics}
+        val_metrics_progress = {ds: {} for ds in val_datasets}
+        for ds in val_metrics_progress:
+            val_metrics_progress[ds] = {metric_name: [] for metric_name in val_metrics}
+        train_loss_mean_progress = {ds_name: [] for ds_name in train_datasets}
+        val_loss_mean_progress = {ds_name: [] for ds_name in val_datasets}
+        train_loss_batch_progress = {ds_name: [] for ds_name in train_datasets}
+
+        # display training progress
+        progress_bar = tqdm(range(num_training_steps))
+        counter = 0
+        display_loss_after_iters = math.ceil(
+            sum(num_steps_per_epoch_per_dataloader.values()) / 100
+        )
+        # define training loss
+        bce_loss = torch.nn.BCELoss()
+
+        # training loop
+        for epoch in range(num_epochs):
+
+            # reshuffle the sampling schedule
+            random.shuffle(samples_schedule)
+            encoder.train()
+            [classifiers[cls_name].train() for cls_name in classifiers]
+            loss_progress = []
+            train_epoch_loss_progress = {ds_name: [] for ds_name in train_datasets}
+
+            dataloader_iterators = {
+                ds_name: iter(train_datasets[ds_name].torch_dataloader)
+                for ds_name in train_datasets
+            }
+
+            for source_ds in samples_schedule:
+                # get batch from the selected data source
+                batch = {
+                    k: v.to(device)
+                    for k, v in next(dataloader_iterators[source_ds]).items()
+                }
+                # forward pass
+                features = encoder(**batch)
+                predictions = classifiers[source_ds].forward(features)
+                # backward pass
+                cls_loss = bce_loss(
+                    predictions, batch["labels"].unsqueeze(1).to(torch.float32)
+                )
+                cls_loss.backward()
+                # optimize the corresponding classifier and encoder
+                cls_optimizers[source_ds].step()
+                encoder_optimizer.step()
+                # update learning rates for all optimizers
+                [lr_scheduler.step() for lr_scheduler in lr_schedulers]
+                # reset gradients
+                cls_optimizers[source_ds].zero_grad()
+                encoder_optimizer.zero_grad()
+                progress_bar.update(1)
+
+                loss_progress.append(cls_loss.item())
+                train_epoch_loss_progress[source_ds].append(cls_loss.item())
+                if counter % display_loss_after_iters == 0:
+                    mean_loss = np.mean(np.array(loss_progress))
+                    print("Training loss:", mean_loss)
+                    loss_progress = []
+
+            # validation
+            encoder.eval()
+            [classifiers[cls_name].eval() for cls_name in classifiers]
+            val_epoch_loss_progress = {ds_name: [] for ds_name in val_datasets}
+
+            for val_ds_name in val_datasets:
+                for batch in val_datasets[val_ds_name].torch_dataloader:
+                    batch = {k: v.to(device) for k, v in batch.items()}
+                    with torch.no_grad():
+                        features = encoder(**batch)
+                        probs = classifiers[val_ds_name].forward(features)
+
+                    cls_loss = bce_loss(
+                        probs, batch["labels"].unsqueeze(1).to(torch.float32)
+                    )
+                    val_epoch_loss_progress[val_ds_name].append(cls_loss.item())
+                    predictions = torch.round(probs)
+                    [
+                        val_metrics[val_metric].add_batch(
+                            predictions=predictions, references=batch["labels"]
+                        )
+                        for val_metric in val_metrics
+                    ]
+                [
+                    val_metrics_progress[val_ds_name][val_metric].append(
+                        val_metrics[val_metric].compute()[val_metric]
+                    )
+                    for val_metric in val_metrics
+                ]
+
+            # compute average losses per epoch
+            for ds_name in train_datasets:
+                train_loss_mean_progress[ds_name].append(
+                    np.mean(np.array(train_epoch_loss_progress[ds_name]))
+                )
+                train_loss_batch_progress[ds_name].append(
+                    train_epoch_loss_progress[ds_name]
+                )
+            train_epoch_loss_progress = {ds_name: [] for ds_name in train_datasets}
+            for ds_name in val_datasets:
+                val_loss_mean_progress[ds_name].append(
+                    np.mean(np.array(val_epoch_loss_progress[ds_name]))
+                )
+            val_epoch_loss_progress = {ds_name: [] for ds_name in val_datasets}
+
+            # save all models from the epoch
+            # encoder
+            save_path = os.path.join(
+                paths.OUTPUT_MODELS_FINETUNNED_ENCODER,
+                "_".join([self.name, start_time, str(epoch)]),
+            )
+            os.makedirs(os.path.split(save_path)[0], exist_ok=True)
+            torch.save(encoder.state_dict(), save_path)
+
+            # classifiers
+            cls_save_paths = [
+                os.path.join(
+                    paths.OUTPUT_MODELS_FINETUNNED_CLASSIFIER,
+                    "_".join([self.name, start_time, cls, str(epoch)]),
+                )
+                for cls in classifiers
+            ]
+            [
+                os.makedirs(os.path.split(path)[0], exist_ok=True)
+                for path in cls_save_paths
+            ]
+            [
+                torch.save(classifiers[cls_name].state_dict(), path)
+                for cls_name, path in zip(classifiers, cls_save_paths)
+            ]
+
+        for ds_name in val_datasets:
+            val_metrics_progress[ds_name]["val_loss"] = val_loss_mean_progress[ds_name]
+            val_metrics_progress[ds_name]["train_loss"] = train_loss_mean_progress[
+                ds_name
+            ]
+        info_save_path = os.path.join(
+            OUTPUT_INFO_FINETUNNING, "_".join(self.name, start_time)
+        )
+        with open(info_save_path, "w") as fp:
+            json.dump(val_metrics_progress, fp)
         pass
 
 
--- src/training/source_finetuning.py
+++ src/training/source_finetuning.py
@@ -17,7 +17,6 @@ from src.model.tokenizers import Tokenizer
 from src.utils.text_preprocessing import Preprocessor
 
 
-
 source_mall = read_mall().sample(7)
 source_facebook = read_facebook().sample(6)
 datasets = [source_facebook, source_mall]
@@ -32,12 +31,22 @@ train_datasets, val_datasets = get_source_datasets_ready_for_finetuning(
     num_workers=0,
 )
 
-
-# load model
-model = RobertaForSequenceClassification.from_pretrained(
-    "ufal/robeczech-base",
-    num_labels=2,
+asc = AdaptiveSentimentClassifier(
+    Preprocessor(), Tokenizer(), Encoder(), ClassificationHead, Discriminator()
 )
+
+asc.finetune(
+    train_datasets,
+    val_datasets,
+    optimizer=AdamW,
+    optimizer_params={"lr": 2e-5, "betas": (0.9, 0.999)},
+    lr_params={"lr_decay": 0.95, "n_layers_following": 1},
+    warmup_steps_proportion=0.1,
+    lr_scheduler_call=get_linear_schedule_with_warmup,
+    num_epochs=4,
+    metrics=["f1", "accuracy", "precision", "recall"],
+)
+
 
 ### training details from the RobeCzech paper
 
@@ -70,65 +79,3 @@ model = RobertaForSequenceClassification.from_pretrained(
 # b1=0.9 and b2=0.999
 # learning rate = 2e-5
 # 4 epochs
-
-
-# specify training details
-optimizer = AdamW(model.parameters(), lr=2e-5, betas(0.9, 0.999))
-num_epochs = 4
-num_training_steps = num_epochs * len(train_dataloader)
-scheduler = get_linear_schedule_with_warmup(
-    optimizer=optimizer,
-    num_warmup_steps=num_training_steps * 0.1,
-    num_training_steps=num_training_steps,
-)
-
-# gpu if available, else cpu
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-model.to(device)
-
-# training loop
-val_metric = load_metric("f1")  # accuracy
-val_metric_progress = []
-
-progress_bar = tqdm(range(num_training_steps))
-counter = 0
-
-for epoch in range(num_epochs):
-
-    # training
-    model.train()
-    loss_progress = []
-    for batch in train_dataloader:
-        counter += 1
-        batch = {k: v.to(device) for k, v in batch.items()}
-        outputs = model(**batch)
-        loss = outputs.loss
-        loss.backward()
-
-        optimizer.step()
-        scheduler.step()
-        optimizer.zero_grad()
-        progress_bar.update(1)
-        loss_progress.append(loss.item())
-        if counter % 5 == 0:
-            mean_loss = np.mean(np.array(loss_progress))
-            print("Training loss:", mean_loss)
-            loss_progress = []
-
-    # validation
-    model.eval()
-    for batch in val_dataloader:
-        batch = {k: v.to(device) for k, v in batch.items()}
-        with torch.no_grad():
-            outputs = model(**batch)
-
-        logits = outputs.logits
-        predictions = torch.argmax(logits, dim=-1)
-        val_metric.add_batch(predictions=predictions, references=batch["labels"])
-
-    val_metric_progress.append(val_metric.compute()["f1"])
-    print("Validation metric:", val_metric_progress[len(val_metric_progress) - 1])
-
-    save_path = "output/models/finetunned/robeczech_" + str(epoch)
-    os.makedirs(os.path.split(save_path)[0], exist_ok=True)
-    torch.save(model.state_dict(), save_path)
