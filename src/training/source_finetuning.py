7f304bde9194b4b7b0173de9e9d43bf8b0f79465
adamcihlar <adam.cihlar3@gmail.com>
2022-11-22 17:35:55 +0100

    Move code to main

--- src/training/source_finetuning.py
+++ src/training/source_finetuning.py
@@ -12,38 +12,38 @@ from src.model.classifiers import (
     Discriminator,
 )
 
+if __name__ == "__main__":
+    source_mall = read_mall().sample(100)
+    source_facebook = read_facebook().sample(100)
+    datasets = [source_facebook, source_mall]
 
-source_mall = read_mall().sample(100)
-source_facebook = read_facebook().sample(100)
-datasets = [source_facebook, source_mall]
+    train_datasets, val_datasets = get_source_datasets_ready_for_finetuning(
+        datasets,
+        drop_neutral=True,
+        preprocessor=Preprocessor(),
+        tokenizer=Tokenizer(),
+        batch_size=8,
+        shuffle=True,
+        num_workers=0,
+    )
 
-train_datasets, val_datasets = get_source_datasets_ready_for_finetuning(
-    datasets,
-    drop_neutral=True,
-    preprocessor=Preprocessor(),
-    tokenizer=Tokenizer(),
-    batch_size=8,
-    shuffle=True,
-    num_workers=0,
-)
+    asc = AdaptiveSentimentClassifier(
+        Preprocessor(),
+        Tokenizer(),
+        Encoder(),
+        ClassificationHead,
+        Discriminator(),
+        Encoder(),
+    )
 
-asc = AdaptiveSentimentClassifier(
-    Preprocessor(),
-    Tokenizer(),
-    Encoder(),
-    ClassificationHead,
-    Discriminator(),
-    Encoder(),
-)
-
-asc.finetune(
-    train_datasets=train_datasets,
-    val_datasets=val_datasets,
-    optimizer=AdamW,
-    optimizer_params={"lr": 2e-5, "betas": (0.9, 0.999)},
-    lr_params={"lr_decay": 0.95, "n_layers_following": 1},
-    lr_scheduler_call=get_linear_schedule_with_warmup,
-    warmup_steps_proportion=0.1,
-    num_epochs=4,
-    metrics=["f1", "accuracy", "precision", "recall"],
-)
+    asc.finetune(
+        train_datasets=train_datasets,
+        val_datasets=val_datasets,
+        optimizer=AdamW,
+        optimizer_params={"lr": 2e-5, "betas": (0.9, 0.999)},
+        lr_params={"lr_decay": 0.95, "n_layers_following": 1},
+        lr_scheduler_call=get_linear_schedule_with_warmup,
+        warmup_steps_proportion=0.1,
+        num_epochs=4,
+        metrics=["f1", "accuracy", "precision", "recall"],
+    )
