5282e706d5fe8d0469b77bbfa6b2d634e2ce45b2
adamcihlar <adam.cihlar3@gmail.com>
2022-11-22 10:01:50 +0100

    Add models paths

--- src/config/paths.py
+++ src/config/paths.py
@@ -28,3 +28,28 @@ OUTPUT = "output"
 
 # PREDICTIONS
 OUTPUT_PREDICTIONS = os.path.join(OUTPUT, "predictions")
+
+# MODELS
+OUTPUT_MODELS = os.path.join(OUTPUT, "models")
+
+# FINETUNNED
+OUTPUT_MODELS_FINETUNNED = os.path.join(OUTPUT_MODELS, "finetunned")
+OUTPUT_MODELS_FINETUNNED_ENCODER = os.path.join(OUTPUT_MODELS_FINETUNNED, "encoder")
+OUTPUT_MODELS_FINETUNNED_CLASSIFIER = os.path.join(
+    OUTPUT_MODELS_FINETUNNED, "clasclassfication_head"
+)
+
+# ADAPTED
+OUTPUT_MODELS_ADAPTED = os.path.join(OUTPUT_MODELS, "adapted")
+OUTPUT_MODELS_ADAPTED_ENCODER = os.path.join(OUTPUT_MODELS_ADAPTED, "encoder")
+OUTPUT_MODELS_ADAPTED_DISCRIMINATOR = os.path.join(
+    OUTPUT_MODELS_ADAPTED, "discriminator"
+)
+
+# TRAIN INFO
+OUTPUT_INFO = os.path.join(OUTPUT, "train_info")
+
+# FINETUNNING
+OUTPUT_INFO_FINETUNNING = os.path.join(OUTPUT_INFO, "finetunning")
+# ADAPTATION
+OUTPUT_INFO_ADAPTATION = os.path.join(OUTPUT_INFO, "adaptation")
