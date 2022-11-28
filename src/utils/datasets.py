e575af7d4da53b4fe5d4cf40ed7953c17f954e06
adamcihlar <adam.cihlar3@gmail.com>
2022-11-28 08:54:53 +0100

    First modify target then select source

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -243,12 +243,13 @@ def get_datasets_ready_for_adaptation(
     shuffle,
     num_workers,
 ):
-    source_train_df, source_val_df, target_df = get_adaptation_datasets(
-        source_train_df, source_val_df, target_df
-    )
     if target_df.label is not None:
         target_df = drop_undefined_classes(target_df)
         target_df = transform_labels_to_probs(target_df, drop_neutral=drop_neutral)
+
+    source_train_df, source_val_df, target_df = get_adaptation_datasets(
+        source_train_df, source_val_df, target_df
+    )
 
     source_train = ClassificationDataset(
         source_train_df.text, source_train_df.label, source_train_df.source
