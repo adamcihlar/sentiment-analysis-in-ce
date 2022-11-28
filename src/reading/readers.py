d2a590decdcc818e7940750418899c4306ef6dfa
adamcihlar <adam.cihlar3@gmail.com>
2022-11-28 07:27:56 +0100

    Read datasets used for source finetuning

--- src/reading/readers.py
+++ src/reading/readers.py
@@ -3,6 +3,7 @@ import os
 from loguru import logger
 
 from src.config import paths
+from src.config import parameters
 
 
 def _check_file(path_to_file):
@@ -38,3 +39,18 @@ def read_all_source(path=paths.DATA_PROCESSED_CONCAT):
         df = pd.read_csv(path, index_col=0)
         return df
     return None
+
+
+def read_finetuning_source(
+    selected_model=parameters.FINETUNED_CHECKPOINT,
+    selected_dataset=parameters.FINETUNED_DATASET,
+):
+    train_path = os.path.join(
+        paths.DATA_FINAL_SOURCE_TRAIN, "_".join([selected_model, selected_dataset])
+    )
+    val_path = os.path.join(
+        paths.DATA_FINAL_SOURCE_VAL, "_".join([selected_model, selected_dataset])
+    )
+    train_ds = pd.read_csv(train_path, index_col=0)
+    val_ds = pd.read_csv(val_path, index_col=0)
+    return train_ds, val_ds
