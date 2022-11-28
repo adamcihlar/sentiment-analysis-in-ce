f0508126b1332e449d1874d687ebbd1f7df93d39
adamcihlar <adam.cihlar3@gmail.com>
2022-11-28 07:28:28 +0100

    Add import

--- src/training/target_adaptation.py
+++ src/training/target_adaptation.py
@@ -2,7 +2,12 @@ from torch.optim import AdamW
 from transformers import get_linear_schedule_with_warmup
 
 from src.config import paths
-from src.reading.readers import read_facebook, read_mall, read_csfd
+from src.reading.readers import (
+    read_facebook,
+    read_mall,
+    read_csfd,
+    read_finetuning_source,
+)
 from src.utils.text_preprocessing import Preprocessor
 from src.model.tokenizers import Tokenizer
 from src.model.encoders import Encoder
@@ -12,8 +17,7 @@ from src.model.classifiers import (
     Discriminator,
 )
 
-source_train_dataset = read_mall().sample(10)
-source_val_dataset = read_mall().sample(4)
+source_train_dataset, source_val_dataset = read_finetuning_source()
 target_dataset = read_csfd().sample(10)
 
 asc = AdaptiveSentimentClassifier(
@@ -23,4 +27,7 @@ asc = AdaptiveSentimentClassifier(
     ClassificationHead,
     Discriminator(),
     Encoder(path_to_finetuned=paths.OUTPUT_MODELS_FINETUNED_ENCODER_FINAL),
+    # paths.OUTPUT_MODELS_FINETUNED_CLASSIFIER_FINAL,
 )
+
+type(acs.classifier)
