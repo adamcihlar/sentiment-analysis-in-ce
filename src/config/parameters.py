ebf794fe73bf976f3170fa9e2daf690728ab14c1
adamcihlar <adam.cihlar3@gmail.com>
2022-11-28 07:27:23 +0100

    Specify selected model after finetuning

--- src/config/parameters.py
+++ src/config/parameters.py
@@ -2,6 +2,6 @@ RANDOM_STATE = 42
 TOKENIZER_MODEL = "ufal/robeczech-base"
 ENCODER_MODEL = "ufal/robeczech-base"
 
-FINETUNED_CHECKPOINT = ""
+FINETUNED_CHECKPOINT = "ufalrobeczech-base_20221127-171131"
 FINETUNED_EPOCH = 3
 FINETUNED_DATASET = "mall"
