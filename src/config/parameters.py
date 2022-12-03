0a7561fa02e26e1e3bae2f5280c80f74af477670
adamcihlar <adam.cihlar3@gmail.com>
2022-12-03 15:39:50 +0100

    Change model

--- src/config/parameters.py
+++ src/config/parameters.py
@@ -1,6 +1,8 @@
 RANDOM_STATE = 42
-TOKENIZER_MODEL = "ufal/robeczech-base"
-ENCODER_MODEL = "ufal/robeczech-base"
+# TOKENIZER_MODEL = "ufal/robeczech-base"
+TOKENIZER_MODEL = "Seznam/small-e-czech"
+# ENCODER_MODEL = "ufal/robeczech-base"
+ENCODER_MODEL = "Seznam/small-e-czech"
 
 FINETUNED_CHECKPOINT = "ufalrobeczech-base_20221127-171131"
 FINETUNED_EPOCH = 3
