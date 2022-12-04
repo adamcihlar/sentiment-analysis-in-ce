4ee4777710e02115785972614465d7285b095a5e
adamcihlar <adam.cihlar3@gmail.com>
2022-12-04 15:39:44 +0100

    Change tanh for relu

--- src/model/classifiers.py
+++ src/model/classifiers.py
@@ -56,7 +56,7 @@ class ClassificationHead(torch.nn.Module):
                 torch.nn.Sequential(
                     torch.nn.Dropout(dropout),
                     torch.nn.Linear(input_size, hidden_size),
-                    torch.nn.Tanh(),
+                    torch.nn.ReLU(),
                 ),
                 torch.nn.Sequential(
                     torch.nn.Dropout(dropout),
