0c2a5a36702158b6a1c70a07952ec68388018f97
adamcihlar <adam.cihlar3@gmail.com>
2023-04-15 15:31:21 +0200

    Target inference testing schedule

--- src/scripts/target_mixed_inference.py
+++ src/scripts/target_mixed_inference.py
@@ -18,6 +18,94 @@ from src.model.encoders import Encoder
 from src.model.tokenizers import Tokenizer
 from src.utils.text_preprocessing import Preprocessor
 
+models = [
+    ("seznamsmall-e-czech_20230218-142950", "5", "mall_facebook", "ordinal"),
+    ("seznamsmall-e-czech_20230218-183829", "5", "facebook_csfd", "ordinal"),
+    ("seznamsmall-e-czech_20230218-213739", "5", "mall_csfd", "ordinal"),
+]
+
+datasets = [read_csfd, read_mall, read_facebook]
+datasets_names = ["csfd", "mall", "facebook"]
+
+emb_layers = [-1, -2]
+
+sim_dist_types = [True, False]
+
+dim_sizes = [None, 0.999, 0.99, 0.98, 0.95, 0.9, 0.8]
+
+labelled_sizes = [30, 50, 100]
+
+for i, model in enumerate(models):
+
+    enc = "_".join([model[0], model[1]])
+    enc_pth = os.path.join(paths.OUTPUT_MODELS_FINETUNED_ENCODER, enc)
+    cls = "_".join([model[0], model[2], model[1]])
+    cls_pth = os.path.join(paths.OUTPUT_MODELS_FINETUNED_CLASSIFIER, cls)
+
+    asc = AdaptiveSentimentClassifier(
+        Preprocessor(),
+        Tokenizer(),
+        Encoder(path_to_finetuned=enc_pth),
+        ClassificationHead,
+        Discriminator(),
+        Encoder(path_to_finetuned=enc_pth),
+        classifier_checkpoint_path=cls_pth,
+        inference_mode=True,
+        task_settings=model[3],
+    )
+
+    target_df = datasets[i]().sample(parameters.AdaptationOptimizationParams.N_EMAILS)
+    target_df.text.to_csv(os.path.join(paths.INPUT, "hello.csv"), index=False)
+    y_true = target_df.label.copy().reset_index(drop=True)
+
+    target_ds = ClassificationDataset(None)
+    target_ds.read_user_input()
+
+    target_ds.preprocess(asc.preprocessor)
+    target_ds.tokenize(asc.tokenizer)
+    target_ds.create_dataset()
+    target_ds.create_dataloader(16, False)
+
+    for layer in emb_layers:
+        for nn in sim_dist_types:
+            for dim_size in dim_sizes:
+
+                # output samples for labelling
+                asc.suggest_anchor_set(target_ds, layer=layer, dim_size=dim_size)
+
+                for samples_labelled in labelled_sizes:
+                    ### THIS WILL BE DONE BY USER
+                    anch = pd.read_csv(
+                        os.path.join(paths.INPUT_ANCHOR, "anchor_set.csv"), index_col=0
+                    )
+                    anch.label.iloc[0:samples_labelled] = y_true.loc[
+                        anch.label.iloc[0:samples_labelled].index
+                    ]
+                    anch.to_csv(os.path.join(paths.INPUT_ANCHOR, "anchor_set.csv"))
+                    ###
+
+                    # read labelled subset
+                    target_ds.read_anchor_set()
+
+                    # bulk inference
+                    y_pred = asc.mix_bulk_predict(target_ds, nn=nn, scale=False)
+
+                    target_ds.y = y_true
+
+                    file_name = (
+                        datasets_names[i]
+                        + str(layer)
+                        + str(nn)
+                        + str(dim_size)
+                        + str(samples_labelled)
+                        + ".json"
+                    )
+                    save_results_path = os.path.join(
+                        paths.OUTPUT_INFO_INFERENCE, file_name
+                    )
+                    target_ds.evaluation_report(save_results_path)
+
+
 # setup
 model = (
     "seznamsmall-e-czech_20230218-142950",
