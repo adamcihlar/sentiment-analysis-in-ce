cb1840c9ecaf9194229270be8133304801418dcb
adamcihlar <adam.cihlar3@gmail.com>
2023-04-18 12:52:02 +0200

    Add import

--- src/model/classifiers.py
+++ src/model/classifiers.py
@@ -34,6 +34,7 @@ from sklearn.decomposition import PCA
 from sklearn.metrics import silhouette_score, silhouette_samples
 from sklearn.cluster import KMeans
 from scipy.spatial import distance_matrix
+from transformers import get_constant_schedule
 
 
 class ClassificationHead(torch.nn.Module):
